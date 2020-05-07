3cea383eccd60ae75f2d7ec05edd4cdd6b1240ba
restuu <restu_u@outlook.com>
2020-05-07 13:56:09 +0700

    Feat: Introduce gin cors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
 	"github.com/tamaApotek/tama-go-server/config"
@@ -24,7 +25,14 @@ func main() {
 	userUsecase := user.NewUsecase(userRepo)
 	doctorUsecase := doctor.NewUsecase(doctorRepo, userRepo)
 
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	// same as
+	// config := cors.DefaultConfig()
+	// config.AllowAllOrigins = true
+	// router.Use(cors.New(config))
+	r.Use(cors.Default())
 
 	user.NewHandler(r.Group("/users"), userUsecase)
 	doctor.NewHandler(r.Group("/doctors"), doctorUsecase)
